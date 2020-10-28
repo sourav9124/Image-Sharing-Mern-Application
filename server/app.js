4bca718911dd721cd6bb833ac46009b80f24232e
Sourav <souravsingh0021@gmail.com>
2020-10-28 17:34:03 +0530

    server files uploaded

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const mongoose=require('mongoose')
 const  {MONGOURI}=require('./keys.js')
-const PORT=5000
+const PORT=500
 mongoose.connect(MONGOURI,
     {
       useNewUrlParser:true,
